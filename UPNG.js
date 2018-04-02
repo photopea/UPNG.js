c1bae3a5746b271c18fbd52251c2aea1cc188b4d
Photopea <support@photopea.com>
2018-04-02 20:48:25 +0200

    Add files via upload

--- UPNG.js
+++ UPNG.js
@@ -11,6 +11,11 @@ function log() { if (typeof process=="undefined" || process.env.NODE_ENV=="devel
 
 	
 
+	
+
+var UPNG = {};
+
+	
 
 UPNG.toRGBA8 = function(out)
 {
@@ -386,19 +391,52 @@ UPNG._copyTile = function(sb, sw, sh, tb, tw, th, xoff, yoff, mode)
 
 
 
-
-
 UPNG.encode = function(bufs, w, h, ps, dels, forbidPlte)
 {
 	if(ps==null) ps=0;
 	if(forbidPlte==null) forbidPlte = false;
-	var data = new Uint8Array(bufs[0].byteLength*bufs.length+100);
+
+	var nimg = UPNG.encode.compress(bufs, w, h, ps, false, forbidPlte);
+	UPNG.encode.compressPNG(nimg, -1);
+	
+	return UPNG.encode._main(nimg, w, h, dels);
+}
+
+UPNG.encodeLL = function(bufs, w, h, cc, ac, depth, dels) {
+	var nimg = {  ctype: 0 + (cc==1 ? 0 : 2) + (ac==0 ? 0 : 4),      depth: depth,  frames: []  };
+	
+	var bipp = (cc+ac)*depth, bipl = bipp * w;
+	for(var i=0; i<bufs.length; i++)
+		nimg.frames.push({  rect:{x:0,y:0,width:w,height:h},  img:new Uint8Array(bufs[i]), blend:0, dispose:1, bpp:Math.ceil(bipp/8), bpl:Math.ceil(bipl/8)  });
+	
+	UPNG.encode.compressPNG(nimg, 4);
+	
+	return UPNG.encode._main(nimg, w, h, dels);
+}
+
+UPNG.encode._main = function(nimg, w, h, dels) {
+	var crc = UPNG.crc.crc, wUi = UPNG._bin.writeUint, wUs = UPNG._bin.writeUshort, wAs = UPNG._bin.writeASCII;
+	var offset = 8, anim = nimg.frames.length>1, pltAlpha = false;
+	
+	var leng = 8 + (16+5+4)  + (9+4) + (anim ? 20 : 0);
+	if(nimg.ctype==3) {
+		var dl = nimg.plte.length;
+		for(var i=0; i<dl; i++) if((nimg.plte[i]>>>24)!=255) pltAlpha = true;
+		leng += (8 + dl*3 + 4) + (pltAlpha ? (8 + dl*1 + 4) : 0);
+	}
+	for(var j=0; j<nimg.frames.length; j++)
+	{
+		var fr = nimg.frames[j];
+		if(anim) leng += 38;
+		leng += fr.cimg.length + 12;
+		if(j!=0) leng+=4;
+	}
+	leng += 12; 
+	
+	var data = new Uint8Array(leng);
 	var wr=[0x89, 0x50, 0x4e, 0x47, 0x0d, 0x0a, 0x1a, 0x0a];
 	for(var i=0; i<8; i++) data[i]=wr[i];
-	var offset = 8,  bin = UPNG._bin, crc = UPNG.crc.crc, wUi = bin.writeUint, wUs = bin.writeUshort, wAs = bin.writeASCII;
-
-	var nimg = UPNG.encode.compressPNG(bufs, w, h, ps, forbidPlte);
-
+	
 	wUi(data,offset, 13);     offset+=4;
 	wAs(data,offset,"IHDR");  offset+=4;
 	wUi(data,offset,w);  offset+=4;
@@ -416,11 +454,10 @@ UPNG.encode = function(bufs, w, h, ps, dels, forbidPlte)
 	data[offset] = 1;  offset++;
 	wUi(data,offset,crc(data,offset-5,5));  offset+=4; // crc
 
-	var anim = bufs.length>1;
 	if(anim) {
 		wUi(data,offset, 8);      offset+=4;
 		wAs(data,offset,"acTL");  offset+=4;
-		wUi(data,offset, bufs.length);      offset+=4;
+		wUi(data,offset, nimg.frames.length);     offset+=4;
 		wUi(data,offset, 0);      offset+=4;
 		wUi(data,offset,crc(data,offset-12,12));  offset+=4; // crc
 	}
@@ -430,16 +467,16 @@ UPNG.encode = function(bufs, w, h, ps, dels, forbidPlte)
 		wUi(data,offset, dl*3);  offset+=4;
 		wAs(data,offset,"PLTE");  offset+=4;
 		for(var i=0; i<dl; i++){
-			var ti=i*3, c=nimg.plte[i], r=(c)&255, g=(c>>8)&255, b=(c>>16)&255;
+			var ti=i*3, c=nimg.plte[i], r=(c)&255, g=(c>>>8)&255, b=(c>>>16)&255;
 			data[offset+ti+0]=r;  data[offset+ti+1]=g;  data[offset+ti+2]=b;
 		}
 		offset+=dl*3;
 		wUi(data,offset,crc(data,offset-dl*3-4,dl*3+4));  offset+=4; // crc
 
-		if(nimg.gotAlpha) {
+		if(pltAlpha) {
 			wUi(data,offset, dl);  offset+=4;
 			wAs(data,offset,"tRNS");  offset+=4;
-			for(var i=0; i<dl; i++)  data[offset+i]=(nimg.plte[i]>>24)&255;
+			for(var i=0; i<dl; i++)  data[offset+i]=(nimg.plte[i]>>>24)&255;
 			offset+=dl;
 			wUi(data,offset,crc(data,offset-dl-4,dl+4));  offset+=4; // crc
 		}
@@ -450,8 +487,8 @@ UPNG.encode = function(bufs, w, h, ps, dels, forbidPlte)
 	{
 		var fr = nimg.frames[j];
 		if(anim) {
-			wUi(data,offset, 26);     offset+=4;
-			wAs(data,offset,"fcTL");  offset+=4;
+			wUi(data, offset, 26);     offset+=4;
+			wAs(data, offset,"fcTL");  offset+=4;
 			wUi(data, offset, fi++);   offset+=4;
 			wUi(data, offset, fr.rect.width );   offset+=4;
 			wUi(data, offset, fr.rect.height);   offset+=4;
@@ -478,18 +515,15 @@ UPNG.encode = function(bufs, w, h, ps, dels, forbidPlte)
 	wAs(data,offset,"IEND");  offset+=4;
 	wUi(data,offset,crc(data,offset-4,4));  offset+=4; // crc
 
-	return data.buffer.slice(0,offset);
+	return data.buffer;
 }
 
-UPNG.encode.compressPNG = function(bufs, w, h, ps, forbidPlte)
-{
-	var out = UPNG.encode.compress(bufs, w, h, ps, false, forbidPlte);
-	for(var i=0; i<bufs.length; i++) {
-		var frm = out.frames[i], nw=frm.rect.width, nh=frm.rect.height, bpl=frm.bpl, bpp=frm.bpp;
-		var fdata = new Uint8Array(nh*bpl+nh);
-		frm.cimg = UPNG.encode._filterZero(frm.img,nh,bpp,bpl,fdata);
-	}	
-	return out;
+UPNG.encode.compressPNG = function(out, filter) {
+	for(var i=0; i<out.frames.length; i++) {
+		var frm = out.frames[i], nw=frm.rect.width, nh=frm.rect.height;
+		var fdata = new Uint8Array(nh*frm.bpl+nh);
+		frm.cimg = UPNG.encode._filterZero(frm.img,nh,frm.bpp,frm.bpl,fdata, filter);
+	}
 }
 
 
@@ -549,7 +583,6 @@ UPNG.encode.compress = function(bufs, w, h, ps, forGIF, forbidPlte)
 	if(cc<=256 && forbidPlte==false) {
 		if(cc<= 2) depth=1;  else if(cc<= 4) depth=2;  else if(cc<=16) depth=4;  else depth=8;
 		if(forGIF) depth=8;
-		gotAlpha = true;
 	}
 	
 	for(var j=0; j<frms.length; j++)
@@ -578,7 +611,7 @@ UPNG.encode.compress = function(bufs, w, h, ps, forGIF, forbidPlte)
 	}
 	//console.log("colors => palette indices", Date.now()-time);  time = Date.now();
 	
-	return {ctype:ctype, depth:depth, plte:plte, gotAlpha:gotAlpha, frames:frms  };
+	return {ctype:ctype, depth:depth, plte:plte, frames:frms  };
 }
 UPNG.encode.framize = function(bufs,w,h,forGIF,brute) {
 	var frms = [];
@@ -626,8 +659,12 @@ UPNG.encode.framize = function(bufs,w,h,forGIF,brute) {
 	return frms;
 }
 
-UPNG.encode._filterZero = function(img,h,bpp,bpl,data)
+UPNG.encode._filterZero = function(img,h,bpp,bpl,data, filter)
 {
+	if(filter!=-1) {
+		for(var y=0; y<h; y++) UPNG.encode._filterLine(data, img, y, bpl, bpp, filter);
+		return pako["deflate"](data);
+	}
 	var fls = [];
 	for(var t=0; t<5; t++) {  if(h*bpl>500000 && (t==2 || t==3 || t==4)) continue;
 		for(var y=0; y<h; y++) UPNG.encode._filterLine(data, img, y, bpl, bpp, t);
@@ -856,6 +893,13 @@ UPNG.encode.concatRGBA = function(bufs, roundAlpha) {
 	return nimg.buffer;
 }
 
+	
+	
+	
+	
+	
+	
+	
 
 
 
